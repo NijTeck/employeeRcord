74b266c65de74e8dbe1d4ca48d1c22449565a62c
Leonard Esere <leonardesere@gmail.com>
2020-07-12 15:29:05 -0500

    fixed errors that prevented code from running.  issues with sql code and the connection section., the bug is resolved, now adding customer styling to the console log sections

--- connection.js
+++ connection.js
@@ -1,19 +1,19 @@
-const mysql = require("mysql");
 const connection = mysql.createConnection({
-    host: "127.0.0.1",
+    host: "localhost",
   
     // Your port;
-    port: 3306,
+    port: 3300,
   
     // Your username
     user: "root",
   
-    password: "Bilon$$555",
-    database: "employee_db"
+    password: "Bilon555",
+    database: "employee_DB"
   });
   connection.connect(function(err) {
     if (err) throw err;
     console.log("connected as id " + connection.threadId + "\n");
     start(); 
   });
-  module.exports = connection
+
+  
---
+++ seed.sql
@@ -0,0 +1,12 @@
+
+USE employee_DB;
+
+INSERT INTO employees (first_name, last_name, role_id, manager_id)
+VALUES ("Jason", "Smith", 1, 2), ("Ellen", "Park", 2, 1), ("Stacy", "Carter", 3, 2);
+
+INSERT INTO roles (title, salary, department_id)
+VALUES ("Manager", 200, 1), ("Engineer", 100, 2), ("Intern", 10, 2);
+
+INSERT INTO departments (name)
+VALUES ("Admin"), ("Engineering"), ("Education");
+
